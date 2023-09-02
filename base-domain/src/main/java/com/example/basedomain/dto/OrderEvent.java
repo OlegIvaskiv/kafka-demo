37730ff89a953d7aed67707fa43164dc45ec43a2
Oleh Ivaskiv <olegivaskiv1@gmail.com>
2023-09-02 19:16:17 +0300

    initial commit

---
+++ base-domain/src/main/java/com/example/basedomain/dto/OrderEvent.java
@@ -0,0 +1,16 @@
+package com.example.basedomain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
